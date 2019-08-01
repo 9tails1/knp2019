fed97ec0dd56963b7b798a5d1e8afe2081893ee9
nitesh <niteshg@miway.co.za>
2019-08-01 21:12:47 +0200

    Detail page per pet invoked with URL param Used PDO bindings to  stop sql injection

--- php03/start/lib/functions.php
+++ php03/start/lib/functions.php
@@ -4,22 +4,40 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 
-function getPets($limit)
-{
+
+function getPDO () {
     $configVars = require_once 'config.php';
     $pdo = new PDO($configVars['database_connection'],$configVars['username'],$configVars['password']);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    return $pdo;
+}
+function getPets($limit = null)
+{
+    $pdo = getPDO();
     $query = 'Select * from pet ';
-    if($limit != 0) {
-        $query .= 'LIMIT '.$limit;
-        $result = $pdo->query($query);
+    if($limit) {
+        $query .= 'LIMIT :resultLimit';
     }
-    $result = $pdo->query($query);
+
+    $result = $pdo->prepare($query);
+    $result->bindParam('resultLimit',$limit, PDO::PARAM_INT);
+    $result->execute();
+
     $pets = $result->fetchAll();
 
-    /*$petsJson = file_get_contents('data/pets.json');
-    $pets = json_decode($petsJson, true);*/
-
     return $pets;
+}
+
+function getPet($id) {
+    $pdo = getPDO();
+    $query = 'Select * from pet where id = :idVal' ;
+    $result = $pdo->prepare($query);
+    $result->bindParam('idVal',$id);
+    $result->execute();
+
+    $pet = $result->fetch();
+
+    return $pet;
 }
 
 function savePets($petsToSave)
